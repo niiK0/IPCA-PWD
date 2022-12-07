e9a66244222fbb1240cf03aeb7f4d3ac9e9347aa
niiK0 <37949374+niiK0@users.noreply.github.com>
2022-12-07 05:51:07 +0000

    A bunch of progress?
    -Background particles with particlesJS
    -Game background particles
    -Resized, recolored images
    -Adjusted variables
    -Improved functions
    -Enemy grids
    -Shooting and losing

---
+++ script/bg.js
@@ -0,0 +1,151 @@
+let drops = []
+let particles = []
+
+const star_colors = ['white', 'yellow', 'orange']
+
+//#region ParticleJS
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 160, density: { enable: true, value_area: 500 } },
+    shape: {type: "star"}, //cirlce maybe?
+    color: { value: ["#fff", "#FFA500", "#FFFF00"] },
+    opacity: {
+      value: 1,
+      random: true,
+      anim: { enable: true, speed: 0.5, opacity_min: 0, sync: false }
+    },
+    size: {
+      value: 2,
+      random: true
+    },
+    line_linked: {
+      enable: false,
+      color: "#FFA500"
+    },
+    move: {
+      enable: true,
+      speed: 0.15,
+      direction: "bottom",
+      random: false,
+      straight: true,
+      out_mode: "out",
+      bounce: false,
+      // attract: { enable: false, rotateX: 600, rotateY: 600 }
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: { enable: true, mode: "grab" },
+      resize: false
+    },
+    modes: {
+      grab: { distance: 100, line_linked: { opacity: 0.4} },
+    }
+  },
+  retina_detect: true
+});
+
+//#endregion
+
+class Particle{
+  constructor({position, velocity, radius, color, fades}){
+    this.position = position
+    this.velocity = velocity
+
+    this.radius = radius
+    this.color = color
+    this.opacity = 1
+    this.fades = fades
+  }
+
+  draw(){
+    c.beginPath()
+    c.globalAlpha = this.opacity
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+    c.fillStyle = this.color
+    c.fill()
+    c.closePath()
+    c.restore()
+  }
+
+  update(){
+    this.draw()
+    this.position.x += this.velocity.x
+    this.position.y += this.velocity.y
+    
+    if(this.fades)
+      this.opacity -= 0.01
+  }
+}
+
+//#region RAIN
+  function rain_setup() {
+    for (let i = 0; i < 25; i++) {
+      drops[i] = new Drop();
+    }
+  }
+  
+  function rain_draw() {
+      for (let i = 0; i < drops.length; i++) {
+        drops[i].fall();
+        drops[i].show();
+      }
+    }
+    
+    function Drop() {
+      this.x = Math.random() * canvas.width;
+      this.y = Math.random() * -500;
+      this.z = Math.random() * 20;
+      this.len = map(this.z, 0, 20, 10, 20);
+      this.yspeed = map(this.z, 0, 20, 1, 100);
+    
+      this.fall = function() {
+        this.y = this.y + this.yspeed;
+        var grav = map(this.z, 0, 20, 0, 0.2);
+        this.yspeed = this.yspeed + grav;
+    
+        if (this.y > canvas.height) {
+          this.y = Math.random() * -500;
+          this.yspeed = map(this.z, 0, 20, 4, 10);
+        }
+      }
+    
+      this.show = function() {
+        var thick = map(this.z, 0, 20, 1, 3);
+        c.lineWidth = thick;
+        c.strokeStyle = 'rgba(255, 255, 255, 0.2)';
+        c.beginPath();
+        c.moveTo(this.x, this.y);
+        c.lineTo(this.x, this.y + this.len);
+        c.stroke();
+      }
+    }
+    
+    // This function maps a value from one range to another
+    function map(value, start1, stop1, start2, stop2) {
+      return start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1));
+    }
+//#endregion
+
+//#region SLOW STARS
+  function stars_setup(){
+    for(let i = 0; i < 100; i++){
+      particles.push(
+        new Particle({
+          position: {
+            x: Math.random() * canvas.width, //random
+            y: Math.random() * canvas.height, //random
+          },
+          velocity: {
+            x: 0,
+            y: 0.07
+          },
+          radius: Math.random() * 2, //random
+          color: star_colors[Math.floor(Math.random() * 3)], //random
+          fades: false
+        })
+      )
+    }
+  }
+//#endregion
--- script/main.js
+++ script/main.js
@@ -1,5 +1,8 @@
+// const canvas = /** @type {HTMLCanvasElement} */ (
+//   document.querySelector("canvas")
+// );
 const canvas = /** @type {HTMLCanvasElement} */ (
-  document.querySelector("canvas")
+  document.getElementById("game-canvas")
 );
 const c = canvas.getContext("2d");
 
@@ -10,6 +13,7 @@ canvas.width = bg_image.width;
 // canvas.width = innerWidth;
 canvas.height = innerHeight;
 
+//#region CLASSES
 class Player {
   constructor() {
     this.velocity = {
@@ -100,23 +104,51 @@ class Projectile {
   }
 }
 
-class Enemy {
-  constructor() {
-    this.velocity = {
-      x: 0,
-      y: 0,
+class EnemyProjectile {
+  constructor({ position, velocity }) {
+    const image = new Image();
+    image.src = "./img/laser2.png";
+    image.onload = () => {
+      const scale = 0.4;
+      this.image = image;
+      this.position = position;
+      this.velocity = velocity;
+      this.width = image.width * scale;
+      this.height = image.height * scale;
     };
+  }
 
+  draw() {
+    c.beginPath();
+    c.rect(this.position.x, this.position.y, this.width, this.height);
+    c.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
+    c.closePath();
+  }
+
+  update() {
+    this.draw();
+    this.position.y += this.velocity;
+  }
+}
+
+class Enemy {
+  constructor({position}) {
     const image = new Image();
     image.src = "./img/enemy.png";
     image.onload = () => {
-      const scale = 0.25;
+      const scale = 1;
       this.image = image;
       this.height = image.height * scale;
       this.width = image.width * scale;
       this.position = {
-        x: canvas.width / 2 - this.width / 2,
-        y: this.width,
+        x: position.x,
+        y: position.y,
       };
     };
   }
@@ -131,25 +163,82 @@ class Enemy {
     );
   }
 
-  update() {
+  update({velocity}) {
     if (this.image) {
       this.draw();
-      this.position.x += this.velocity.x
-      this.position.y += this.velocity.y
-
-
+      this.position.x += velocity.x;
+      this.position.y += velocity.y;
     }
+  }
+
+  shoot(enemy_projectiles){
+    enemy_projectiles.push(new EnemyProjectile({
+      position: {
+        x: this.position.x + this.width / 2 - 3, //3 pixels to adjust the position
+        y: this.position.y + this.height
+      },
+      velocity: 5
+    }))
   }
 }
 
-const player = new Player();
-const enemies = [new Enemy()];
-const enemy = new Enemy()
-var player_shoot_interval = 500
-var enemy_shoot_interval = 500
+class Grid {
+  constructor() {
+    this.position = {
+      x: 0,
+      y: 0,
+    };
+
+    this.velocity = {
+      x: 3,
+      y: 0,
+    };
+
+    this.enemies = [];
+
+    const columns = Math.floor(Math.random() * 5 + 5); //random
+    const rows = Math.floor(Math.random() * 5 + 2); //random
+
+    this.width = columns * enemy_fake_width
+    this.height = rows * enemy_fake_height
+
+    for (let x = 0; x < columns; x++) {
+      for (let y = 0; y < rows; y++) {
+        this.enemies.push(
+          new Enemy({
+            position: {
+              x: x * enemy_fake_width,
+              y: y * enemy_fake_height
+            },
+          })
+        )
+
+      }
+    }
+  }
+
+  update(){
+    this.position.x += this.velocity.x
+    this.position.y += this.position.x
+
+    this.velocity.y = 0
+
+    if(this.position.x + this.width >= canvas.width || this.position.x <= 0){
+      this.velocity.x = -this.velocity.x
+      this.velocity.y = enemy_fake_height
+    }
+  }
+}
+//#endregion
+
+
+//PLAYER VARS
+const player = new Player()
+
+let player_shoot_frames = 1
+let player_shoot_interval = 50;
 
 const projectiles = [];
-const enemy_projectiles = [];
 
 const keys = {
   a: {
@@ -164,94 +253,190 @@ const keys = {
   },
 };
 
+//ENEMY VARS
+const grids = []
+let grid_frames = 0
+let grid_spawn_interval = Math.floor(Math.random() * 1000 + 1000)
+
+let enemy_shoot_frames = 1
+let enemy_shoot_interval = 200;
+
+let enemy_fake_width = 34
+let enemy_fake_height = 28
+
+const enemy_projectiles = [];
+
+
 function animate() {
   requestAnimationFrame(animate);
-  
+
   //Background
   c.drawImage(bg_image, 0, 0, canvas.width, canvas.height);
 
   //black bg to fade
-  c.fillStyle = 'rgba(0, 0, 0, 0.8)';
+  c.fillStyle = "rgba(0, 0, 0, 0.8)";
   c.fillRect(0, 0, canvas.width, canvas.height);
-  
-  //draw the "rain" for stars effect
-  rain_draw()
 
-  player.update();
-  enemy.update();
+  //draw the "rain" for stars effect
+  rain_draw();
+  
+  particles.forEach((particle, i) => {
+    if (particle.position.y - particle.radius >= canvas.height) {
+      particle.position.x = Math.random() * canvas.width; //random
+      particle.position.y = -particle.radius;
+    }
+
+    if (particle.opacity <= 0) {
+      setTimeout(() => {
+        particles.splice(i, 1);
+      }, 0);
+    } else {
+      particle.update();
+    }
+  });
+
+  //#region KEYS MOVE/SHOOT
+    if (keys.a.pressed && player.position.x >= 10) {
+      player.velocity.x = -7;
+      player.rotation = -0.15;
+    } else if (
+      keys.d.pressed &&
+      player.position.x + player.width <= canvas.width - 10
+    ) {
+      player.velocity.x = 7;
+      player.rotation = 0.15;
+    } else {
+      player.velocity.x = 0;
+      player.rotation = 0;
+    }
+  
+    if (keys.space.pressed) {
+      if (keys.space.can_shoot) {
+        keys.space.can_shoot = false;
+        projectiles.push(
+          new Projectile({
+            position: {
+              x: player.position.x + player.width / 2 - 3, //3 pixels to adjust the position
+              y: player.position.y,
+            },
+            velocity: -10,
+          })
+        );
+      }
+    }
+    //#endregion
 
   projectiles.forEach((projectile, index) => {
-    if(projectile.position.y + projectile.height <= 0){
-        setTimeout(() =>{
-            projectiles.splice(index, 1)
-        }, 0)
-    }else{
-        projectile.update();
+    if (projectile.position.y + projectile.height <= 0) {
+      setTimeout(() => {
+        const projectile_found = projectiles.find(projectile2 => projectile2 === projectile)
+        if(projectile_found){
+          projectiles.splice(index, 1);
+        }
+      }, 0);
+    } else {
+      projectile.update();
     }
   });
 
   enemy_projectiles.forEach((projectile, index) => {
-    if(projectile.position.y + projectile.height >= canvas.height){
-        setTimeout(() =>{
-          enemy_projectiles.splice(index, 1)
-        }, 0)
-    }else{
-        projectile.update();
+    if (projectile.position.y + projectile.height >= canvas.height) {
+      setTimeout(() => {
+        const projectile_found = enemy_projectiles.find(projectile2 => projectile2 === projectile)
+        if(projectile_found){
+          enemy_projectiles.splice(index, 1);
+        }
+      }, 0);
+    } else {
+      projectile.update();
+    }
+
+    //PLAYER COLLISION
+    if(projectile.position.y + projectile.height >= player.position.y
+      && projectile.position.x + projectile.width >= player.position.x
+      && projectile.position.x <= player.position.x + player.width
+      ){
+      //TAKE PLAYER LIVES
+      console.log("you died")
     }
   });
 
-  if (keys.a.pressed && player.position.x >= 10) {
-    player.velocity.x = -7;
-    player.rotation = -0.15;
-  } else if (
-    keys.d.pressed &&
-    player.position.x + player.width <= canvas.width - 10
-  ) {
-    player.velocity.x = 7;
-    player.rotation = 0.15;
-  } else {
-    player.velocity.x = 0;
-    player.rotation = 0;
+  grids.forEach((grid, grid_index) => {
+    grid.update();
+
+    if(enemy_shoot_frames % enemy_shoot_interval === 0 && grid.enemies.length > 0){
+      grid.enemies[Math.floor(Math.random() * grid.enemies.length)].shoot(enemy_projectiles) //random
+      enemy_shoot_frames = 0
+    }
+
+    grid.enemies.forEach((enemy, i) => {
+      enemy.update({velocity: grid.velocity})
+
+      projectiles.forEach((projectile, j) =>{
+        if(projectile.position.y <= enemy.position.y + enemy_fake_height
+          && projectile.position.x + projectile.width >= enemy.position.x
+          && projectile.position.x <= enemy.position.x + enemy_fake_width
+          && projectile.position.y + projectile.width >= enemy.position.y
+          ){
+          setTimeout(()=>{
+            const enemy_found = grid.enemies.find(enemy2 => enemy2 === enemy)
+            const projectile_found = projectiles.find(projectile2 => projectile2 === projectile)
+            
+            if(enemy_found && projectile_found){
+              grid.enemies.splice(i, 1)
+              projectiles.splice(j, 1)
+
+              if(grid.enemies.length > 0){
+                const first_enemy = grid.enemies[0]
+                const last_enemy = grid.enemies[grid.enemies.length - 1]
+
+                grid.width = last_enemy.position.x - first_enemy.position.x + last_enemy.width
+                grid.position.x = first_enemy.position.x
+              }else{
+                grids.splice(grid_index,1)
+              }
+            }
+          }, 0)
+        }
+      })
+    });
+  });
+
+  if(grid_frames % grid_spawn_interval === 0){
+    grids.push(new Grid())
+    grid_spawn_interval = Math.floor(Math.random() * 1000 + 1000)
+    grid_frames = 0
   }
 
-  if(keys.space.pressed){
-    if(keys.space.can_shoot){
-        keys.space.can_shoot = false
-        projectiles.push(new Projectile({
-            position: {
-                x: player.position.x + player.width / 7,
-                y: player.position.y + player.height
-            },
-            velocity: -10
-        }))
+  player.update();
+
+  //FUN GLITCH LOL
+  // enemy_projectiles.forEach((projectile, index) => {
+  //   if (projectile.position.y + projectile.height >= canvas.height) {
+  //     setTimeout(() => {
+  //       enemy_projectiles.splice(index, 1);
+  //     }, 0);
+  //   } else {
+  //     projectile.update();
+  //   }
+  // });
+
+  //#region PLAYER SHOOTING COOLDOWN
+  if (player_shoot_frames % player_shoot_interval === 0) {
+    if (!keys.space.can_shoot) {
+      keys.space.can_shoot = true;
+      player_shoot_frames = 1
     }
   }
+  //#endregion
+
+  //increment frames for update values
+  grid_frames++
+  player_shoot_frames++;
+  enemy_shoot_frames++;
 }
 
-function shotCooldown(){
-  setInterval(() => {
-      if(!keys.space.can_shoot){
-          keys.space.can_shoot = true
-      }
-    }, player_shoot_interval)
-}
-
-function enemyShotCooldown(){
-  setInterval(() => {
-    enemies.forEach((enemy) =>{
-      enemy_projectiles.push(new Projectile({
-        position: {
-            x: enemy.position.x + enemy.width / 7,
-            y: enemy.position.y - 10
-        },
-        velocity: 10
-      }))
-    })
-    }, enemy_shoot_interval)
-}
-enemyShotCooldown()
-shotCooldown()
-
+//#region KEY LISTENERS
 addEventListener("keydown", ({ key }) => {
   switch (key) {
     case "a":
@@ -279,8 +464,14 @@ addEventListener("keyup", ({ key }) => {
       break;
   }
 });
+//#endregion
 
 animate();
 
-//Raindrop
+//#region BACKGROUND PARTICLES SETUP
+//RAINDROP
 rain_setup();
+
+//STARS
+stars_setup();
+//#endregion
